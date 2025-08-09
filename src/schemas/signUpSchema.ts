cff313d21129baa69170adfa9ecd26f4f128cbcd
LuvSharma007 <luvsharma9910@gmail.com>
2025-08-09 22:54:15 +0530

    created user and message model , zod validation

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,15 @@
+import {z} from 'zod'
+
+
+export const usernameValidation = z
+    .string()
+    .min(3,"Username must be altest 2 characters")
+    .max(20,"Username must be no more than 20 characters")
+    .regex(/^[a-zA-Z0-9_]+$/,"username must not contain special character")
+
+
+export const signUpSchema = z.object({
+    username:usernameValidation,
+    email:z.string().email({message:"Invalid email address"}),
+    password:z.string().min(8,{message:"Password must be atleast 8 characters"})
+})
